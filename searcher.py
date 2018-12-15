18f03cae73aaa8bded44ecd8bac3766de93f854f
bulongo <bmukkuli@gmail.com>
2018-12-15 15:00:33 +0200

    initial commit

---
+++ searcher.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup as bs
+import requests, sys
+
+#A scipt to scrape sites and download the content depending on what file it type it is
+#i.e. if it is a movie ask to download it or solmething of the sort
+
+script,site = sys.argv
+
+print(site)
