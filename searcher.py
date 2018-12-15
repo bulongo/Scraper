30dd7ef98d1ade7b4aca96e4912cb05e3831c21c
bulongo <bmukkuli@gmail.com>
2018-12-15 15:07:39 +0200

    Progress

--- searcher.py
+++ searcher.py
@@ -4,6 +4,11 @@ import requests, sys
 #A scipt to scrape sites and download the content depending on what file it type it is
 #i.e. if it is a movie ask to download it or solmething of the sort
 
-script,site = sys.argv
+site = input("Enter a site to start scraping: ")
 
-print(site)
+holder = requests.get(site)
+
+soup = bs("lxml",holder.text)
+
+for links in soup.findAll("a"):
+    print(links)
