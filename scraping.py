4e1ad0a771e61416893c23567e36deed5d9ef36e
bulongo <bmukkuli@gmail.com>
2018-12-18 00:50:22 +0200

    New version

--- scraping.py
+++ scraping.py
@@ -1,23 +1,33 @@
+#Version 0.2
+#this version aims to use a different approach
+
 from bs4 import BeautifulSoup as bs
-import requests, sys, webbrowser
+import requests
 
 #A scipt to scrape sites and download the page of a site and then sends it to webtopdf
 
-site = input("Enter a site to start scraping: ")
+www = "www."
+site = ""
+new_site = ''
 
-holder = requests.get(site)
+def site_getter():
+    global site,new_site
+    ext = "http://"
+    choice = input("Would you like to add the http extension? (Y/N): ")
+    if choice.capitalize() == "Y":
+        site = input("Enter a site name: ")
+        #kept having problems becuase i was using a local variable gloabally
+        new_site = ext + www + site
+    else:
+        site = input("Enter name: ")
+        new_site = www + site
+
+site_getter()
+
+holder = requests.get(new_site)
 
 soup = bs(holder.text,"lxml")
 
-links = []
+for links in soup.findAll("a"):
+    print(links.text)
 
-'''for items in soup.findAll("a"):
-    links.append(items.text)
-    if items.isalpha():
-        print(items.text)
-
-#for things in links:
-#    print(things) '''
-
-for items in soup.findAll("title"):
-    print(items)
